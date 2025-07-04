887a17ba995e4c056d672bed1d3e363c42e86110
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2025-07-04 10:57:39 +0500

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+
+// alert("hello") // we are using nodeJs not browser
+
+console.log(3 
+    + 4
+); // code readability should be high
+
+
+let name = "Sameed"
+let age = 17
+let isLoggedIn = true
+
+// number => 2 to power of 53
+// bigint
+// string => "", '', ``
+// boolean => true/false
+// null => *standAlone value* (empty)
+// undefined => in future fill it
+// symbol => unique value
+
+// Object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
