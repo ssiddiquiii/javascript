4b2bab6d6c0ef06a33939bcc91a2821a4d561f12
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2025-07-05 19:07:48 +0500

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,32 @@
+// console.log(null);
+
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+/*
+Note: == and <,>,<=,>= both work differently with null
+equlity and comparision noth have differnet syntax
+*/
+
+// console.log(null > 0); // (false) convert null into 0
+// console.log(null == 0); // (fasle) do not convert null into 0
+// console.log(null >= 0); // (true) convert null into 0
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
