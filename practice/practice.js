b21063ea373efdfabc7e81892071e04f64488d4c
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2025-07-10 19:05:13 +0500

    practice

--- practice/practice.js
+++ practice/practice.js
@@ -93,10 +93,57 @@ let valueInNumber = Number(score)
 
 let someNumber = 100
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // *********************************** Operators ************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "Sameed"
+let str2 = " Siddiqui"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + "2" + "2");
+// console.log(1 + 2 + "2");
+
+let num1, num2, num3
+num1 = num2 = num3 = 5
+// console.log(num2);
+
+// Assignment of PreFix and Postfix operators
+// PostFix
+
+// let bond = 12
+// let jamesBond = bond++
+
+// console.log(bond);
+// console.log(jamesBond);
+
+
+// Postfix
+let bond = 12
+let jamesBond = ++bond
+console.log(bond);
+console.log(jamesBond);
+
+
+
+
 
 
 
