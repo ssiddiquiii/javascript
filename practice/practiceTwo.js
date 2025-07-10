8639a3c0f6e209446277a0d0c521120f392fb7e0
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2025-07-10 19:13:04 +0500

    practice

---
+++ practice/practiceTwo.js
@@ -0,0 +1,32 @@
+// ======== Chapter: Comparison 
+
+// console.log(2 < 2);
+// console.log(2 <= 2);
+// console.log(2 > 2);
+// console.log(2 >= 2);
+// console.log(2 == 2);
+// console.log(2 === 2);
+// console.log(2 != 2);
+// console.log(2 !== 2);
+
+
+
+// =============== Null
+
+// console.log(null > -2); // convert null into 0 that why 0 is greater than -2 = true
+// console.log(null >= -2);
+// console.log(null == 0); // do not covert null into 0
+
+/*
+Note: == and <,>,<=,>= both work differently with null,
+equlity and comparision both have differnet syntax
+*/
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined >= 0);
+
+// ===
+
+// console.log(2 === "2");
+
