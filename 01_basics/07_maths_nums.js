e10c25909a950a49f4b67e6ce146ed4987239fe6
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2025-07-14 14:44:41 +0500

    maths and numbers

---
+++ 01_basics/07_maths_nums.js
@@ -0,0 +1,53 @@
+const score = 400
+// console.log(score);
+
+const balace = new Number(100)
+// console.log(balace);
+
+// console.log(balace.toString().length);
+// console.log(balace.toFixed(1));
+
+const otherNumber = 123.98878
+// console.log(otherNumber.toPrecision(5));
+
+let hunderds = 10000000
+// console.log(hunderds.toLocaleString("en-PK"));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++ Maths //
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // convert any negative value into absolute
+// console.log(Math.round(100.90));
+// console.log(Math.ceil(100.90));
+// console.log(Math.floor(100.90));
+// console.log(Math.sqrt(25));
+
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1);
+
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
