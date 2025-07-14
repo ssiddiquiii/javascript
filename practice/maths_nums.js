e4768468f1566eba0ed0027fe7e0170807b42624
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2025-07-14 15:17:27 +0500

    math practice

---
+++ practice/maths_nums.js
@@ -0,0 +1,54 @@
+// ========= Numbers
+
+const score = 100.897
+
+/*
+console.log(score);
+console.log(score.toString().length);
+console.log(score.toPrecision(4));
+console.log(score.toFixed(2));
+*/
+
+
+const newNumber = new Number(200000.234)
+// console.log(newNumber.__proto__);
+// console.log(newNumber.toLocaleString("en-PK"));
+
+
+// +++++++++++++++++++++ Maths
+
+/*
+console.log(Math);
+console.log(Math.abs(-10));
+console.log(Math.round(100.56));
+console.log(Math.ceil(100.56));
+console.log(Math.floor(100.56));
+console.log(Math.sqrt(25));
+*/
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1); // zero nhi aega ab plus 1 se
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min) + min));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
